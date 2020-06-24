e8051ac5b5b4f10912ddb22edbad534c356a52f6
wagada <492857052@qq.com>
2020-06-24 17:59:58 +0800

    题库后台项目

---
+++ src/pages/question/classification/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <a-tabs default-active-key="1" @change="callback">
+      <a-tab-pane key="1" tab="科目分类">
+        <Subject />
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="章节考点分类" force-render>
+        2222
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="前端分类">
+        3333
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+<script>
+import Subject from './subject'
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    callback(key) {
+      console.log(key);
+    },
+  },
+  components:{
+    Subject,
+  }
+};
+</script>
